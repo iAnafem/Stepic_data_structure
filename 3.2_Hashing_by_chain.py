07dec6813b5d88e32d22f603cf885bac8f822e7a
Пронькин Дмитрий Юрьевич <DPronkin@mostro.ru>
2019-04-29 11:38:09 +0300

    3_2 task complete

--- 3.2_Hashing_by_chain.py
+++ 3.2_Hashing_by_chain.py
@@ -1,41 +1,44 @@
 class ChainHash:
-    def __init__(self, _hash):
-        self._hash = _hash
+    def __init__(self, _dict):
+        self._dict = _dict
         self.x = 263
         self.mod = 1000000007
-        self.output = list()
 
     def pow(self, degree):
-        result = self.x
-        for i in range(degree - 1):
-            result = result * self.x % self.mod
-        return result
+        if degree == 0:
+            return 1
+        elif degree == 1:
+            return self.x
+        elif degree % 2 == 0:
+            result = self.pow(degree / 2)
+            return (result * result) % self.mod
+        else:
+            return (self.pow(degree - 1) * self.x) % self.mod
 
     def calc_hash(self, string):
-        result = ord(string[0])
-        for i in range(1, len(string)):
-            result += ord(string[i]) * self.pow(i)
-        result = (result % self.mod) % len(string)
-        return result
+        _hash = 0
+        for i in range(len(string)):
+            _hash += ((ord(string[i]) * self.pow(i)) % self.mod)
+        return _hash % self.mod % len(self._dict)
 
     def add_string(self, string):
         if self.find_string(string) == "yes":
             return
         else:
-            self._hash[self.calc_hash(string)].insert(0, string)
+            self._dict[self.calc_hash(string)].insert(0, string)
 
     def del_string(self, string):
         if self.find_string(string) == "yes":
-            self._hash[self.calc_hash(string)].remove(string)
+            self._dict[self.calc_hash(string)].remove(string)
 
     def find_string(self, string):
-        if string in self._hash[self.calc_hash(string)]:
+        if string in self._dict[self.calc_hash(string)]:
             return "yes"
         else:
             return "no"
 
     def check_string(self, i):
-        print(*self._hash[i])
+        print(*self._dict[i])
 
 
 def main():
@@ -57,6 +60,7 @@ def main():
 
 
 def tests(c):
+    assert c.calc_hash("GooD") == 2
     assert c.calc_hash("luck") == 2
     assert c.calc_hash("world") == 4
     assert c.calc_hash("HellO") == 4
